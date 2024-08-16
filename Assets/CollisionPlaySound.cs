a00f6555c84762c4295e4f6f5718552897895744
Binhou <81512727+shellyChen21@users.noreply.github.com>
2024-08-16 11:32:40 +0800

    add sound and adjust decal

--- Assets/CollisionPlaySound.cs
+++ Assets/CollisionPlaySound.cs
@@ -1,18 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
+using PrimeTween;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class CollisionPlaySound : MonoBehaviour
 {
-    
-    void Start()
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private float delayTime;
+    [SerializeField] private DecalProjector decal;
+
+    private void Start()
     {
-        
+        if (!audioSource)
+            return;
+
+        audioSource.clip = audioClip;
     }
 
-    
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (decal != null)
+            Tween.Custom(0, 1, 2, onValueChange: newValue => decal.fadeFactor = newValue);
+        StartCoroutine(PlaySound());
+    }
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(delayTime);
+        audioSource.Play();
     }
 }
